d7677a614a7969055e03c34955c572e8a9cbb7df
aravindan2020 <61485230+aravindan2020@users.noreply.github.com>
2022-11-09 08:56:26 +0530

    Add files via upload

--- queue.c
+++ queue.c
@@ -1,55 +1,79 @@
-/* Simple Queue Program in C*/
-/* Data Structure Programs,C Array Examples */
-
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 #include<stdlib.h>
-
-#define MAX_SIZE 100
-
-int main() {
-    int item, choice, i;
-    int arr_queue[MAX_SIZE];
-    int rear = 0;
-    int front = 0;
-    int exit = 1;
-
-    printf("\nSimple Queue Example - Array");
-    do {
-        printf("\n\n Queue Main Menu");
-
-        printf("\n1.Insert \n2.Remove \n3.Display \nOthers to exit");
-        printf("\nEnter Your Choice : ");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                if (rear == MAX_SIZE)
-                    printf("\n## Queue Reached Max!!");
-                else {
-                    printf("\nEnter The Value to be Insert : ");
-                    scanf("%d", &item);
-                    printf("\n## Position : %d , Insert Value  : %d ", rear + 1, item);
-                    arr_queue[rear++] = item;
-                }
-                break;
-            case 2:
-                if (front == rear)
-                    printf("\n## Queue is Empty!");
-                else {
-                    printf("\n## Position : %d , Remove Value  : %d ", front, arr_queue[front]);
-                    front++;
-                }
-                break;
-            case 3:
-                printf("\n## Queue Size : %d ", rear);
-                for (i = front; i < rear; i++)
-                    printf("\n## Position : %d , Value  : %d ", i, arr_queue[i]);
-                break;
-            default:
-                exit = 0;
-                break;
-        }
-    } while (exit);
-
-    return 0;
+#define MAX 50
+void insert();
+void delete();
+void display();
+int queue_array[MAX];
+int rear = - 1;
+int front = - 1;
+int main()
+{
+int choice;
+while (1)
+{
+printf("1.Insert element to queue n");
+printf("2.Delete element from queue n");
+printf("3.Display all elements of queue n");
+printf("4.Quit n");
+printf("Enter your choice : ");
+scanf("%d", &choice);
+switch(choice)
+{
+case 1:
+insert();
+break;
+case 2:
+delete();
+break;
+case 3:
+display();
+break;
+case 4:
+exit(1);
+default:
+printf("Wrong choice n");
+}
+}
+}
+void insert()
+{
+int item;
+if(rear == MAX - 1)
+printf("Queue Overflow n");
+else
+{
+if(front== - 1)
+front = 0;
+printf("Inset the element in queue : ");
+scanf("%d", &item);
+rear = rear + 1;
+queue_array[rear] = item;
+}
+}
+void delete()
+{
+if(front == - 1 || front > rear)
+{
+printf("Queue Underflow n");
+return;
+}
+else
+{
+printf("Element deleted from queue is : %dn", queue_array[front]);
+front = front + 1;
+}
+}
+void display()
+{
+int i;
+if(front == - 1)
+printf("Queue is empty n");
+else
+{
+printf("Queue is : n");
+for(i = front; i <= rear; i++)
+printf("%d ", queue_array[i]);
+printf("\n");
+}
 }
